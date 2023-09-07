3af8db8a7c6304874d3cd78712386e3193e1b52d
Abdullahi Salihu <131313531+SiteBldrs@users.noreply.github.com>
2023-09-08 00:10:39 +0100

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,7 +10,7 @@ import {
 } from "components";
 import { fetchWord } from "utils";
 
-let defaultWord = "design";
+let defaultWord = "natural";
 
 export const AppLayout = () => {
   const [isPlaying, setIsPlaying] = useState(false);
